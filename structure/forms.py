45d8b0dbf6e75d1bb5da725e3709ca417ebeef57
carolinesimpson <csimpson@cigionline.org>
2014-01-10 10:20:06 -0500

    Changing labels on contact form.

--- structure/forms.py
+++ structure/forms.py
@@ -4,5 +4,5 @@ from django import forms
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
-    sender = forms.EmailField()
-    cc_myself = forms.BooleanField(required=False)
+    sender = forms.EmailField(label="Your Email")
+    cc_myself = forms.BooleanField(required=False, label="CC Your Email")
