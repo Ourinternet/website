c17486b2266dd344ee49b47bff17b20e176accd6
carolinesimpson <csimpson@cigionline.org>
2014-01-20 13:18:40 -0500

    Adding favicon and apple-touch-icon references.

--- ourinternet/urls.py
+++ ourinternet/urls.py
@@ -7,9 +7,8 @@ urlpatterns = patterns('',
     url(r'', include('structure.urls')),
 
     (r'^robots\.txt$', include('robots.urls')),
-    
+
     (r'^pages/', include('django.contrib.flatpages.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-    # (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/img/favicon.ico'}),
 )
