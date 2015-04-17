bf0abc3c1a645b743f1b21ec93177ecbef7865b2
Caroline Simpson <csimpson@cigionline.org>
2015-04-17 11:02:36 -0400

    Adding outbound link event tracking.

--- tweet_cache/utils.py
+++ tweet_cache/utils.py
@@ -31,19 +31,29 @@ def process_tweets(tweets, account):
 
         for hashtag in hashtags:
             original_tag = "#%s" % hashtag['text']
-            markup = "<a class='twitter-link hashtag' href='https://twitter.com%s'>%s</a>" % (original_tag,
-                                                                                              original_tag)
+            markup = "<a class='twitter-link hashtag' " \
+                     "onclick='trackOutboundLink(\"https://twitter.com%s\");' " \
+                     "href='https://twitter.com%s'>%s</a>" % (original_tag,
+                                                              original_tag,
+                                                              original_tag)
             tweet_text = tweet_text.replace(original_tag, markup)
 
         for link in links:
             original_link = link['url']
-            markup = "<a class='twitter-link' href='%s'>%s</a>" % (link['expanded_url'], original_link)
+            markup = "<a class='twitter-link' " \
+                     "onclick='trackOutboundLink(\"%s\");'  " \
+                     "href='%s'>%s</a>" % (link['expanded_url'],
+                                           link['expanded_url'],
+                                           original_link)
             tweet_text = tweet_text.replace(original_link, markup)
 
         for user_mention in user_mentions:
             original_mention = "@%s" % user_mention['screen_name']
-            markup = "<a class='twitter-link user-mention' href='https://twitter.com/%s'>%s</a>" % (original_mention,
-                                                                                                    original_mention)
+            markup = "<a class='twitter-link user-mention' " \
+                     "onclick='trackOutboundLink(\"https://twitter.com%s\");' " \
+                     "href='https://twitter.com/%s'>%s</a>" % (original_mention,
+                                                               original_mention,
+                                                               original_mention)
             tweet_text = tweet_text.replace(original_mention, markup)
 
         cached_tweet, created = Tweet.objects.get_or_create(tweet_id=tweet.id,
