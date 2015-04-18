426b8d1b99e8f31963cb72f5d021ed1a83a80a01
Caroline Simpson <csimpson@cigionline.org>
2015-04-18 12:45:52 -0400

    Add alias handling to publication views.

--- commission/views.py
+++ commission/views.py
@@ -4,6 +4,7 @@ from .models import PressRelease, Publication, Webcast
 from random import randint
 from django.conf import settings
 from django.contrib.flatpages.models import FlatPage
+from structure.models import UrlAlias
 
 
 def press_release(request, slug, template="commission/press_release.html"):
@@ -22,12 +23,18 @@ class PublicationPageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(PublicationPageView, self).get_context_data(**kwargs)
 
+        try:
+            publication = Publication.objects.get(slug=kwargs['slug'])
+        except Publication.DoesNotExist:
+            alias = UrlAlias.objects.get(source='publication/{}'.format(kwargs['slug']))
+            publication = Publication.objects.get(slug=alias.destination[:13])
+
         footer_page, created = FlatPage.objects.get_or_create(url='/footer/')
         video_number = randint(1, 3)
         context['video_number'] = video_number
         context['GA_SITE_ID'] = settings.GA_SITE_ID
         context['GA_SITE_URL'] = settings.GA_SITE_URL
-        context['publication'] = Publication.objects.get(slug=kwargs['slug'])
+        context['publication'] = publication
         context['footer_page'] = footer_page
         return context
 
